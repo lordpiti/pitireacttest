695eca919bed1a04463e41163d1df4bab91f0ed3
Pablo Amil <eltiodelassortijas@gmail.com>
2018-08-20 00:13:07 +0100

    small fix for google authentication

--- src/Football/Authentication/GoogleAuthenticator.js
+++ src/Football/Authentication/GoogleAuthenticator.js
@@ -1,47 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import apiInstance from '../../axios-test';
 
-const googleLoginButton = ( props ) => {
+class GoogleAuthenticator extends Component {
 
-    const responseGoogle = (response) => {
-    console.log(response.tokenId);
-    const url = 'user/LoginGoogle';
+    render() {
 
-    apiInstance.post(url, { userId: '', accessToken: response.tokenId })
-        .then(responseApi =>{
-            console.log(responseApi);
-            localStorage.setItem('role_react', responseApi.data.role);
-    });
-    props.authenticationTokenUpdate(response.tokenId);
-    }
+        const responseGoogle = (response) => {
+            console.log(response.tokenId);
+            const url = 'user/LoginGoogle';
+    
+            apiInstance.post(url, { userId: '', accessToken: response.tokenId })
+                .then(responseApi =>{
+                    console.log(responseApi);
+                    localStorage.setItem('role_react', responseApi.data.role);
+            });
+            this.props.authenticationTokenUpdate(response.tokenId);
+        }
+    
+        const logout = () => {
+            this.props.authenticationTokenUpdate(null);
+        }
 
-    const logout = (response) => {
-        props.authenticationTokenUpdate(null);
-    }
+        const forceMyOwnLogout = ((response) => {
+            if (window.gapi) {
+                const auth2 = window.gapi.auth2.getAuthInstance()
+                if (auth2 != null) {
+                    auth2.signOut().then(x => {
+                        auth2.disconnect().then(res => {
+                            logout();
+                        });
+                    });
+                }
+            }
+            logout();
+            this.forceUpdate()
+        });
 
-    let buttonLoginLogout = null;
+        let buttonLoginLogout = null;
 
-    if (!props.authenticationToken) {
-        buttonLoginLogout = 
-        <GoogleLogin
-            clientId="357813264391-bc51b2u0ohaeb6v78k2b2tpr5pdi6c09.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-        />
-    }
-    else {
-        buttonLoginLogout = <GoogleLogout buttonText="Logout" onLogoutSuccess={logout} onFailure={logout} />
-    }
+        if (!this.props.authenticationToken) {
+            buttonLoginLogout = 
+            <GoogleLogin style={{color: 'white', backgroundColor: 'blue'}}
+                clientId="357813264391-bc51b2u0ohaeb6v78k2b2tpr5pdi6c09.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+            ><span> Login with Google</span>
+            </GoogleLogin>
+        }
+        else {
+            buttonLoginLogout = <button onClick={forceMyOwnLogout}>Logout</button>
+        }
 
     return (
+        
         <div>
-            {props.authenticationToken}
+            {this.props.authenticationToken}      
             {buttonLoginLogout}
         </div>
   )
+}
 
 }
 
-export default googleLoginButton;
+export default GoogleAuthenticator;
