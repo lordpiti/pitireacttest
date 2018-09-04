916b5fcb5dff6797649f953a1e9d9b8140987f94
Pablo Amil <eltiodelassortijas@gmail.com>
2018-09-04 21:57:03 +0100

    dropzone css tweaks

--- src/Football/components/BasicDropzone/BasicDropzone.js
+++ src/Football/components/BasicDropzone/BasicDropzone.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
+import './BasicDropzone.css';
 
 class BasicDropzone extends Component {
     constructor(props) {
@@ -15,24 +16,41 @@ class BasicDropzone extends Component {
     }
   
     render() {
+
+      let selectedFilesContent = null;
+
+      if (this.state.files && this.state.files.length>0) {
+        selectedFilesContent =   
+        <aside>
+          <h3>Selected Files</h3>
+          <div className="row droppedFileInfo">
+            {
+              this.state.files.map(f => 
+              <div className="col-sm-12" key={f.name}>
+                <div className="row align-items-center">
+                  <div className="col-sm-3">
+                  <img src={f.preview} height="40" width="40" />
+                  </div>
+                  <div className="col-sm-9">
+                  <span className="fileInfo">{f.name} - {f.size} bytes</span>
+                  </div>
+                </div>
+                
+                
+              </div>)
+            }        
+          </div>
+        </aside>
+      }
+
       return (
         <section>
           <div className="dropzone">
             <Dropzone onDrop={(files) => this.onDrop(files)} multiple={this.props.settings.multipleFiles}>
-              <p>Try dropping some files here, or click to select files to upload.</p>
+              <p>Select an image</p>
             </Dropzone>
           </div>
-          <aside>
-            <h2>Dropped files</h2>
-            <ul>
-              {
-                this.state.files.map(f => 
-                <li key={f.name}>
-                  <img src={f.preview} height="80" width="80" />{f.name} - {f.size} bytes
-                </li>)
-              }
-            </ul>
-          </aside>
+          {selectedFilesContent}
         </section>
       );
     }
