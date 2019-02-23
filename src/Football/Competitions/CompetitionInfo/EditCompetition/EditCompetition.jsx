69922ffab010940fd39329f5e2ba145bf42c4e58
Pablo Amil <lordpiti@gmail.com>
2019-02-23 13:14:38 +0000

    added modal for competitions edit

---
+++ src/Football/Competitions/CompetitionInfo/EditCompetition/EditCompetition.jsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { TextField, FormControl, InputLabel, MenuItem, FormHelperText, Select } from '@material-ui/core';
+import editModal from '../../../components/EditModal/editModal';
+import BasicDropzone from '../../../components/BasicDropzone/BasicDropzone';
+import FormValidator from '../../../utilities/FormValidator';
+
+
+
+class EditCompetitionInfo extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		const validRegEx = (range) => /^\d{4}(-\d{4})$/.test(range);
+
+		this.validator = new FormValidator([
+			{
+				field: 'name',
+				method: 'isEmpty',
+				validWhen: false,
+				message: 'Name is required'
+			},
+			{
+				field: 'season',
+				method: validRegEx,
+				validWhen: true,
+				message: 'Season is required and need to have the right format'
+			}
+		]);
+
+		this.state = {
+			...this.state,
+			competitionData: props.competitionData,
+			validation: this.validator.valid(),
+			currentImage: null
+		};
+
+		this.submitted = false;
+	}
+
+	callbackDropzone = (files) => {
+		let fileToUpload = null;
+		files.forEach(file => {
+			const reader = new FileReader();
+			reader.onload = () => {
+				// do whatever you want with the file content
+				fileToUpload = reader.result;
+
+				this.setState({
+					currentImage: {
+						data: fileToUpload,
+						fileName: 'test.png'
+					}
+				})
+			};
+			reader.onabort = () => console.log('file reading was aborted');
+			reader.onerror = () => console.log('file reading has failed');
+			reader.readAsDataURL(file);
+		});
+	}
+
+	dropzoneSettings = {
+		multipleFiles: false,
+		callback: this.callbackDropzone
+	}
+
+	handleInputChange = event => {
+		event.preventDefault();
+
+		this.setState({
+			competitionData: {
+				...this.state.competitionData,
+				[event.target.name]: event.target.value
+			}
+		});
+	}
+
+	handleFormSubmit = event => {
+		event.preventDefault();
+		const validation = this.validator.validate(this.state.competitionData);
+		this.setState({ validation });
+
+		this.submitted = true;
+
+		if (validation.isValid) {
+			let image = null;
+
+			if (this.state.currentImage) {
+				image = {
+					data: this.state.currentImage.data,
+					fileName: this.state.currentImage.fileName
+				};
+			}
+			this.props.saveCompetition(image, this.state.competitionData);
+			this.props.handleClose();
+		}
+	}
+
+	render() {
+		const { classes } = this.props;
+
+		let validation = this.submitted ?         // if the form has been submitted at least once
+			this.validator.validate(this.state.competitionData) :   // then check validity every time we render
+			this.state.validation                   // otherwise just use what's in state
+
+		if (!this.props.competitionData) {
+			return <div></div>
+		} else
+			return (
+				<div>
+					<h1>Competition Basic Info</h1>
+					<div className="row">
+						<div className="col-sm-7">
+							<form>
+								<TextField
+									required
+									error={validation.name.message}
+									id="name"
+									name="name"
+									label="Competition name"
+									defaultValue={this.state.competitionData.name}
+									className={classes.textField}
+									margin="normal"
+									onChange={this.handleInputChange}
+								/>
+								<FormHelperText>Required</FormHelperText>
+								<TextField
+									required
+									error={validation.season.message}
+									id="season"
+									name="season"
+									label="Season"
+									defaultValue={this.state.competitionData.season}
+									className={classes.textField}
+									margin="normal"
+									onChange={this.handleInputChange}
+								/>
+								<FormHelperText>Required</FormHelperText>
+							</form>
+						</div>
+						<div className="col-sm-5 text-center">
+							<img className="roundedImage" src={this.props.competitionData.logo.url} height="100" width="100" />
+							<BasicDropzone settings={this.dropzoneSettings} />
+						</div>
+					</div>
+					<div className="text-right margin-top-medium">
+						<Button variant="contained" color="primary" className={classes.button} onClick={this.handleFormSubmit}>
+							Save
+            </Button>
+					</div>
+				</div>
+			);
+	}
+}
+
+EditCompetitionInfo.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default editModal(EditCompetitionInfo);
