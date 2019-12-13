1c5171b23f809c7fbc440ac8c394540d2bd1f479
Pablo Amil <lordpiti@gmail.com>
2019-12-13 18:29:31 +0000

    used redux hooks in PlayerStatistics component

--- src/Football/Players/PlayerStatistics/PlayerStatistics.jsx
+++ src/Football/Players/PlayerStatistics/PlayerStatistics.jsx
@@ -1,32 +1,39 @@
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Helpers from '../../utilities/helpers';
 import ExpansionPanel from './ExpansionPanel/ExpansionPanel';
 import * as actionCreators from '../../store/actions/global';
 
-class PlayerStatistics extends Component {
+const PlayerStatistics = (props) => {
 
-  componentDidMount() {
-    if (!this.props.isLoading) {
-      this.props.showHideSpinner(true);
+  const { match } = props;
+
+  useEffect(() => {
+    if (!ui.isLoading) {
+      dispatch(actionCreators.updateLoadingSpinner(true));
     }
-  }
+  },  []);
 
-  render() {
-    const playerId = this.props.match.params.id;
+  const ui = useSelector(state => ({
+    isLoading: state.global.loading
+  }));
 
-    const GET_PLAYER_STATISTICS = gql`
-      query PlayerStatistics($playerId: Int!) {
-        player(id: $playerId) {
-          name, surname
-          playerMatchesPlayed {
-              localTeamName, visitorTeamName, id, localGoals, visitorGoals, date, round,
-              competition {
-                id, name, season, type
-              }
+  const dispatch = useDispatch();
+
+  const playerId = match.params.id;
+
+  const GET_PLAYER_STATISTICS = gql`
+    query PlayerStatistics($playerId: Int!) {
+      player(id: $playerId) {
+        name, surname
+        playerMatchesPlayed {
+          localTeamName, visitorTeamName, id, localGoals, visitorGoals, date, round,
+          competition {
+            id, name, season, type
           }
+        }
       }
     }
   `;
@@ -45,58 +52,43 @@ class PlayerStatistics extends Component {
 //   }
 // `;
 
-    return (
-      <Query
-        query={GET_PLAYER_STATISTICS} variables={{playerId}}
-      >
-        {({ loading, error, data }) => {
-          if (loading) {
-            return <p>Loading...</p>;
-          } 
-          if (error) { 
-            return <p>Error :(</p>;
+  return (
+    <Query
+      query={GET_PLAYER_STATISTICS} variables={{playerId}}
+    >
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <p>Loading...</p>;
+        } 
+        if (error) { 
+          return <p>Error :(</p>;
+        }
+
+        if (ui.isLoading) {
+          dispatch(actionCreators.updateLoadingSpinner(false));
+        }
+
+        let groupedCompetitions = Helpers.groupBy(data.player.playerMatchesPlayed, 'competition.id');
+
+        const allCompetitions = data.player.playerMatchesPlayed.map(x => x.competition);
+
+        const uniqueCompetitions = Helpers.removeDuplicates(allCompetitions, 'id');
+
+        let matchListGroupedByCompetition = Object.entries(groupedCompetitions).map(group => {
+          return {
+            competition: uniqueCompetitions.find(x => group[0] == x.id),
+            data: group[1]
           }
+        });
 
-          if (this.props.isLoading) {
-            this.props.showHideSpinner(false);
-          }
-
-          let groupedCompetitions = Helpers.groupBy(data.player.playerMatchesPlayed, 'competition.id');
-
-          const allCompetitions = data.player.playerMatchesPlayed.map(x => x.competition);
-
-          const uniqueCompetitions = Helpers.removeDuplicates(allCompetitions, 'id');
-
-          let matchListGroupedByCompetition = Object.entries(groupedCompetitions).map(group => {
-            return {
-              competition: uniqueCompetitions.find(x => group[0] == x.id),
-              data: group[1]
-            }
-          });
-
-          return (
-            <div>
-              <h1>Games played</h1>
-              <ExpansionPanel matchListGroupedByCompetition={matchListGroupedByCompetition} {...this.props}></ExpansionPanel>
-            </div>)
-        }}
-      </Query>
-
-    );
-
-  }
+        return (
+          <div>
+            <h1>Games played</h1>
+            <ExpansionPanel matchListGroupedByCompetition={matchListGroupedByCompetition} {...props}></ExpansionPanel>
+          </div>)
+      }}
+    </Query>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    isLoading: state.global.loading
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    showHideSpinner: (isLoading) => dispatch(actionCreators.updateLoadingSpinner(isLoading))
-  }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(PlayerStatistics);
+export default PlayerStatistics;
