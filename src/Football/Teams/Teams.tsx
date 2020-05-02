aa70496cab0b2922c514c748157e5a1c9a0e8136
Pablo Amil <lordpiti@gmail.com>
2020-05-02 02:58:03 +0100

    more types

---
+++ src/Football/Teams/Teams.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import TeamDetails from './TeamDetails/TeamDetails';
+import TeamsOverview from './TeamsOverview/TeamsOverview';
+import { RouteComponentProps } from 'react-router';
+
+const Teams = (props: RouteComponentProps) => {
+    return <div className="teams">
+        <Route path={props.match.url+"/"} exact component={TeamsOverview} />
+        <Route path={props.match.url+'/team-details/:id'} component={TeamDetails} />
+    </div>
+}
+
+export default Teams;
