27da452e45c65be655440629e15f6925dfebcea9
Pablo Amil <lordpiti@gmail.com>
2020-05-02 02:49:16 +0100

    added sass

--- src/Football/Teams/TeamSquad/SimpleCard/SimpleCard.tsx
+++ src/Football/Teams/TeamSquad/SimpleCard/SimpleCard.tsx
@@ -11,7 +11,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import Formatters from '../../../utilities/formatters';
-import './SimpleCard.css';
+import './SimpleCard.scss';
 
 const styles = {
   card: {
