cfd5fc691c48d717f4365f5250a203a579791eff
jloborod <jloborod@gmail.com>
2020-05-07 12:35:50 +0100

    fixed CompetitionDetailsHooks test

--- src/Football/Competitions/CompetitionDetails/CompetitionDetailsHooks.test.tsx
+++ src/Football/Competitions/CompetitionDetails/CompetitionDetailsHooks.test.tsx
@@ -1,39 +1,36 @@
-import React from 'react';
-import CompetitionDetailsHooks from './CompetitionDetailsHooks';
-import { shallow, mount } from 'enzyme';
-import { MemoryRouter } from 'react-router';
+import React from "react";
+import CompetitionDetailsHooks from "./CompetitionDetailsHooks";
+import { shallow, mount } from "enzyme";
+import { MemoryRouter } from "react-router";
 
-jest.mock('react-redux', () => ({
+jest.mock("react-redux", () => ({
   useDispatch: () => {},
   useSelector: () => ({
-    currentCompetition: { currentCompetition: {} },
+    currentCompetition: {
+      logo: {
+        url: "super",
+      },
+    },
   }),
+  connect: () => () => {},
 }));
 
-jest.mock('react-router', () => ({
-  Route: jest.fn().mockReturnValue(<div>c</div>),
-}));
-
-describe('<SDGDG /> with all data', () => {
+describe("<SDGDG /> with all data", () => {
   it(`should render correctly`, () => {
     const props = {
       match: {
         id: 2,
-        url: '/',
-        params: { id: '1' },
+        url: "/",
+        params: { id: "1" },
         isExact: false,
-        path: '/',
+        path: "/",
       },
       history: {} as any,
       location: {} as any,
     };
 
-    const component = (
-      <MemoryRouter initialEntries={['/']}>
-        <CompetitionDetailsHooks {...props} />
-      </MemoryRouter>
-    );
-    const metricsPanel = shallow(component as any);
-    expect(metricsPanel).toMatchSnapshot();
+    const wrapper = shallow(<CompetitionDetailsHooks {...props} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
