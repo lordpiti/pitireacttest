9fffeb56385c875e9169e6293b910dafdd17e115
Pablo Amil <lordpiti@gmail.com>
2020-05-17 03:28:15 +0100

    more changes

--- src/Football/Teams/TeamDetails/TeamDetails.tsx
+++ src/Football/Teams/TeamDetails/TeamDetails.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { Route, Redirect, RouteComponentProps } from 'react-router-dom';
 import TeamInfo from '../TeamInfo/TeamInfo';
 import ComplexForm from '../ComplexForm/ComplexForm';
@@ -19,118 +19,112 @@ interface TeamsDetailsProps extends RouteComponentProps<TeamsDetailsParams> {
   currentTeam: any;
 }
 
-class TeamDetails extends Component<TeamsDetailsProps> {
-  componentDidMount() {
-    this.props.loadTeam(this.props.match.params.id);
-  }
+const TeamDetails = (props: TeamsDetailsProps) => {
+  useEffect(() => {
+    props.loadTeam(props.match.params.id);
+  }, []);
 
-  render() {
-    let content,
-      menuContent = null;
+  let content,
+    menuContent = null;
 
-    if (this.props.currentTeam) {
-      menuContent = (
-        <div className='margin-bottom-medium'>
-          <img
-            src={this.props.currentTeam.pictureLogo.url}
-            className='roundedImage'
-            height='50'
-            width='50'
-            alt=''
-          />
-          <span>{this.props.currentTeam.name}</span>
-        </div>
-      );
-      content = (
-        <div>
-          <Route
-            path={this.props.match.url + '/'}
-            exact
-            render={() => <Redirect to={this.props.match.url + '/news'} />}
-          />
-          <Route
-            path={this.props.match.url + '/overview'}
-            component={() => {
-              return <TeamInfo teamData={this.props.currentTeam}></TeamInfo>;
-            }}
-          />
+  if (props.currentTeam) {
+    menuContent = (
+      <div className='margin-bottom-medium'>
+        <img
+          src={props.currentTeam.pictureLogo.url}
+          className='roundedImage'
+          height='50'
+          width='50'
+          alt=''
+        />
+        <span>{props.currentTeam.name}</span>
+      </div>
+    );
+    content = (
+      <div>
+        <Route
+          path={props.match.url + '/'}
+          exact
+          render={() => <Redirect to={props.match.url + '/news'} />}
+        />
+        <Route
+          path={props.match.url + '/overview'}
+          component={() => {
+            return <TeamInfo teamData={props.currentTeam}></TeamInfo>;
+          }}
+        />
 
-          <Route
-            path={this.props.match.url + '/news'}
-            component={() => {
-              return <TeamNews teamData={this.props.currentTeam}></TeamNews>;
-            }}
-          />
+        <Route
+          path={props.match.url + '/news'}
+          component={() => {
+            return <TeamNews teamData={props.currentTeam}></TeamNews>;
+          }}
+        />
 
-          <Route
-            path={this.props.match.url + '/team-squad'}
-            render={() => {
-              return (
-                <TeamSquad
-                  players={this.props.currentTeam.playerList}
-                ></TeamSquad>
-              );
-            }}
-          />
+        <Route
+          path={props.match.url + '/team-squad'}
+          render={() => {
+            return (
+              <TeamSquad players={props.currentTeam.playerList}></TeamSquad>
+            );
+          }}
+        />
 
-          <Route
-            path={this.props.match.url + '/team-stadium'}
-            render={(props) => {
-              return (
-                <TeamStadium
-                  stadium={this.props.currentTeam.stadium}
-                ></TeamStadium>
-              );
-            }}
-          />
+        <Route
+          path={props.match.url + '/team-stadium'}
+          render={() => {
+            return (
+              <TeamStadium stadium={props.currentTeam.stadium}></TeamStadium>
+            );
+          }}
+        />
 
-          <Route
-            path={this.props.match.url + '/complex-form-sample'}
-            component={() => {
-              return <ComplexForm id={teamId}></ComplexForm>;
-            }}
-          />
-        </div>
-      );
-    }
-
-    const teamId = this.props.match.params.id;
-
-    const menuList = [
-      {
-        name: 'News',
-        url: this.props.match.url + '/news',
-      },
-      {
-        name: 'Summary',
-        url: this.props.match.url + '/overview',
-      },
-      {
-        name: 'Squad',
-        url: this.props.match.url + '/team-squad',
-      },
-      {
-        name: 'Stadium',
-        url: this.props.match.url + '/team-stadium',
-      },
-      // {
-      //   name: 'Complex form sample',
-      //   url: this.props.match.url + '/complex-form-sample',
-      // },
-    ];
-
-    return (
-      <div className='Teams'>
-        <div className='row'>
-          <div className='col-sm-3'>
-            <SideMenu itemList={menuList}>{menuContent}</SideMenu>
-          </div>
-          <div className='col-sm-9'>{content}</div>
-        </div>
+        <Route
+          path={props.match.url + '/complex-form-sample'}
+          component={() => {
+            return <ComplexForm id={teamId}></ComplexForm>;
+          }}
+        />
       </div>
     );
   }
-}
+
+  const teamId = props.match.params.id;
+
+  const menuList = [
+    {
+      name: 'News',
+      url: props.match.url + '/news',
+    },
+    {
+      name: 'Summary',
+      url: props.match.url + '/overview',
+    },
+    {
+      name: 'Squad',
+      url: props.match.url + '/team-squad',
+    },
+    {
+      name: 'Stadium',
+      url: props.match.url + '/team-stadium',
+    },
+    // {
+    //   name: 'Complex form sample',
+    //   url: this.props.match.url + '/complex-form-sample',
+    // },
+  ];
+
+  return (
+    <div className='Teams'>
+      <div className='row'>
+        <div className='col-sm-3'>
+          <SideMenu itemList={menuList}>{menuContent}</SideMenu>
+        </div>
+        <div className='col-sm-9'>{content}</div>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state: FootballState) => {
   return {
--- src/Football/Teams/Teams.tsx
+++ src/Football/Teams/Teams.tsx
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import TeamDetails from './TeamDetails/TeamDetails';
 import TeamsOverview from './TeamsOverview/TeamsOverview';
 import { RouteComponentProps } from 'react-router';
 
 const Teams = (props: RouteComponentProps) => {
-    return <div className="teams">
-        <Route path={props.match.url+"/"} exact component={TeamsOverview} />
-        <Route path={props.match.url+'/team-details/:id'} component={TeamDetails} />
+  return (
+    <div className='teams'>
+      <Route path={props.match.url + '/'} exact component={TeamsOverview} />
+      <Route
+        path={props.match.url + '/team-details/:id'}
+        component={TeamDetails}
+      />
     </div>
-}
+  );
+};
 
 export default Teams;
