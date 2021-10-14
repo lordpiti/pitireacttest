b893e4f6a16d20005a08abe3764596d05a721b41
Pablo Amil <lordpiti@gmail.com>
2021-10-14 11:18:20 +0100

    testtinymce

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-
+import { Editor } from '@tinymce/tinymce-react';
 import { Button, ButtonProps } from './Button';
 
 export default {
@@ -12,7 +12,74 @@ export default {
   },
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+  return <>
+    <Button {...args} />
+    <Editor
+      onInit={(evt, editor) => editorRef.current = editor}
+      initialValue="<p>This is the initial content of the editor.</p>"
+      init={{
+        height: 500,
+        menubar: false,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount'
+        ],
+        toolbar: 'undo redo | formatselect | ' +
+          'bold italic backcolor image | alignleft aligncenter ' +
+          'alignright alignjustify | bullist numlist outdent indent | ' +
+          'removeformat | help',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        file_picker_types: 'image',
+        file_picker_callback: function (callback, value, meta) {
+          var input = document.createElement('input');
+          input.setAttribute('type', 'file');
+          input.setAttribute('accept', 'image/*');
+
+          /*
+            Note: In modern browsers input[type="file"] is functional without
+            even adding it to the DOM, but that might not be the case in some older
+            or quirky browsers like IE, so you might want to add it to the DOM
+            just in case, and visually hide it. And do not forget do remove it
+            once you do not need it anymore.
+          */
+
+          input.onchange = function () {
+            var file = this.files[0];
+
+            var reader = new FileReader();
+            reader.onload = function () {
+              /*
+                Note: Now we need to register the blob in TinyMCEs image blob
+                registry. In the next release this part hopefully won't be
+                necessary, as we are looking to handle it internally.
+              */
+              var id = 'blobid' + (new Date()).getTime();
+              var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+              var base64 = reader.result.split(',')[1];
+              var blobInfo = blobCache.create(id, file, base64);
+              blobCache.add(blobInfo);
+
+              /* call the callback and populate the Title field with the file name */
+              callback(blobInfo.blobUri(), { title: file.name });
+            };
+            reader.readAsDataURL(file);
+
+          }
+          input.click();
+        }
+      }}
+    />
+    <button onClick={log}>Log editor content</button>
+  </>
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
