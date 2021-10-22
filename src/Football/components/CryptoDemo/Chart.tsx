4681afc210a2fcb6f878ea0ea3eacabc966b96fe
Pablo Amil <lordpiti@gmail.com>
2021-10-22 18:29:25 +0200

    updated to use the api properly

--- src/Football/components/CryptoDemo/Chart.tsx
+++ src/Football/components/CryptoDemo/Chart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Bar,
   XAxis,
@@ -23,192 +23,7 @@ const colors = [
   '#17becf',
 ];
 
-// const rawData1 = [
-//   {
-//     high: 100,
-//     low: 0,
-//     open: 40,
-//     close: 60,
-//     ts: 1562791020,
-//   },
-//   {
-//     high: 100,
-//     low: 0,
-//     open: 60,
-//     close: 40,
-//     ts: 1562792020,
-//   },
-//   {
-//     high: 70,
-//     low: 20,
-//     open: 25,
-//     close: 50,
-//     ts: 1562793020,
-//   },
-// ];
-
-const rawData = [
-  {
-    high: 7.18811,
-    low: 7.18127,
-    open: 7.18631,
-    close: 7.183,
-    ts: 1562790720,
-  },
-  {
-    high: 7.21184,
-    low: 7.20139,
-    open: 7.20139,
-    close: 7.21138,
-    ts: 1562790780,
-  },
-  {
-    high: 7.21808,
-    low: 7.21524,
-    open: 7.2168,
-    close: 7.21675,
-    ts: 1562790840,
-  },
-  {
-    high: 7.19661,
-    low: 7.19343,
-    open: 7.19633,
-    close: 7.1936,
-    ts: 1562790900,
-  },
-  {
-    high: 7.18131,
-    low: 7.17473,
-    open: 7.17819,
-    close: 7.18131,
-    ts: 1562790960,
-  },
-  {
-    high: 7.17874,
-    low: 7.17475,
-    open: 7.17874,
-    close: 7.17604,
-    ts: 1562791020,
-  },
-  {
-    high: 7.19077,
-    low: 7.18493,
-    open: 7.18684,
-    close: 7.19077,
-    ts: 1562791080,
-  },
-  {
-    high: 7.1837,
-    low: 7.17899,
-    open: 7.1792,
-    close: 7.18246,
-    ts: 1562791140,
-  },
-  {
-    high: 7.18788,
-    low: 7.18098,
-    open: 7.18338,
-    close: 7.18788,
-    ts: 1562791200,
-  },
-  {
-    high: 7.20103,
-    low: 7.19715,
-    open: 7.19778,
-    close: 7.19715,
-    ts: 1562791260,
-  },
-  {
-    high: 7.21353,
-    low: 7.20752,
-    open: 7.20873,
-    close: 7.21116,
-    ts: 1562791320,
-  },
-];
-
-// const rawData3 = [
-//   {
-//     high: 11808.494,
-//     low: 11775.101,
-//     open: 11790.496,
-//     close: 11808.089,
-//     ts: 1562791020,
-//   },
-//   {
-//     high: 11846.242,
-//     low: 11802.6,
-//     open: 11821.585,
-//     close: 11843.063,
-//     ts: 1562791080,
-//   },
-//   {
-//     high: 11829.147,
-//     low: 11803.613,
-//     open: 11803.613,
-//     close: 11827.385,
-//     ts: 1562791140,
-//   },
-//   {
-//     high: 11814.498,
-//     low: 11802.755,
-//     open: 11814.498,
-//     close: 11803.577,
-//     ts: 1562791200,
-//   },
-//   {
-//     high: 11829.527,
-//     low: 11807.739,
-//     open: 11809.03,
-//     close: 11829.527,
-//     ts: 1562791260,
-//   },
-//   {
-//     high: 11855.419,
-//     low: 11825.036,
-//     open: 11830.531,
-//     close: 11835.936,
-//     ts: 1562791320,
-//   },
-//   {
-//     high: 11811.733,
-//     low: 11774.447,
-//     open: 11803.679,
-//     close: 11783.754,
-//     ts: 1562791380,
-//   },
-//   {
-//     high: 11790.889,
-//     low: 11770.107,
-//     open: 11789.994,
-//     close: 11787.824,
-//     ts: 1562791440,
-//   },
-//   {
-//     high: 11794.769,
-//     low: 11757.06,
-//     open: 11794.769,
-//     close: 11775.199,
-//     ts: 1562791500,
-//   },
-//   {
-//     high: 11759.249,
-//     low: 11732.995,
-//     open: 11759.249,
-//     close: 11735.828,
-//     ts: 1562791560,
-//   },
-//   {
-//     high: 11734.435,
-//     low: 11720.716,
-//     open: 11721.952,
-//     close: 11722.716,
-//     ts: 1562791620,
-//   },
-// ];
-
 const Candlestick = (props: any) => {
-  debugger;
   const {
     //fill,
     x,
@@ -222,7 +37,7 @@ const Candlestick = (props: any) => {
   const isGrowing = open < close;
   const color = isGrowing ? 'green' : 'red';
   const ratio = Math.abs(height / (open - close));
-  console.log(props);
+
   return (
     <g stroke={color} fill='none' strokeWidth='2'>
       <path
@@ -279,9 +94,14 @@ const prepareData = (data: any) => {
   });
 };
 
-const CustomShapeBarChart = ({ candleData }: any) => {
+const CustomShapeBarChart = ({
+  candleData,
+  updateDataCallback,
+  symbols,
+  currentSymbol,
+}: any) => {
   const data = prepareData(candleData);
-  data.reduce((acc: any, item: any) => console.log(item), 0);
+  // data.reduce((acc: any, item: any) => console.log(item), 0);
   const minValue = data.reduce(
     (
       minValue: any,
@@ -303,11 +123,19 @@ const CustomShapeBarChart = ({ candleData }: any) => {
     minValue
   );
 
-  console.log(data);
-  console.log(minValue, maxValue);
-
   return (
     <div style={{ height: '900px' }}>
+      <select
+        name='symbol'
+        value={currentSymbol}
+        onChange={(event: any) => {
+          updateDataCallback(event.target.value);
+        }}
+      >
+        {symbols.map((item: string) => (
+          <option value={item}>{item}</option>
+        ))}
+      </select>
       <ResponsiveContainer width='100%' height='100%'>
         <ComposedChart
           // width={600}
--- src/Football/components/CryptoDemo/index.tsx
+++ src/Football/components/CryptoDemo/index.tsx
@@ -18,86 +18,59 @@ interface Props {
 
 const CryptoDemo = (props: Props) => {
   const [estao, setEstao] = useState(null as any);
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  const [currentSymbol, setCurrentSymbol] = useState('BTCUSDT');
+  const [symbols, setSymbols] = useState([] as string[]);
 
-  useEffect(() => {
-    const getit = async () => {
-      const hh = await axiosInstance.get(
-        'https://localhost:44300/weatherforecast'
-      );
-      // const items = (hh as any).data.items.map((x: any) => ({
-      //   name: new Date(x.item1).getTime(),
-      //   pv: x.item2,
-      //   uv: 0,
-      //   amt: 2,
-      // }));
+  const getData = async (symbol: string) => {
+    setCurrentSymbol(symbol);
+    const response = await axiosInstance.get(
+      `https://localhost:44300/api/trading/klines/${symbol}`
+    );
 
-      const candles = (hh as any).data.candles.map((x: any, index: number) => ({
+    // const items = (hh as any).data.items.map((x: any) => ({
+    //   name: new Date(x.item1).getTime(),
+    //   pv: x.item2,
+    //   uv: 0,
+    //   amt: 2,
+    // }));
+
+    const candles = (response as any).data.candles.map(
+      (x: any, index: number) => ({
         high: x.high,
         low: x.low,
         open: x.open,
         close: x.close,
         ts: x.date,
-        ema: (hh as any).data.emaList[index].ema,
-        ema2: (hh as any).data.emaList2[index].ema,
-      }));
+        ema: (response as any).data.emaList[index].ema,
+        ema2: (response as any).data.emaList2[index].ema,
+      })
+    );
 
-      const emaList = (hh as any).data.emaList.map((x: any) => ({
-        high: x.high,
-        low: x.low,
-        open: x.open,
-        close: x.close,
-        ts: new Date(x.date).getTime(),
-      }));
-      const fulobj = { items: emaList, candles: candles };
+    const emaList = (response as any).data.emaList.map((x: any) => ({
+      high: x.high,
+      low: x.low,
+      open: x.open,
+      close: x.close,
+      ts: new Date(x.date).getTime(),
+    }));
+    const fulobj = { items: emaList, candles: candles };
 
-      setEstao(fulobj);
-    };
+    setEstao(fulobj);
+  };
 
-    getit();
+  useEffect(() => {
+    getData(currentSymbol);
+  }, [currentSymbol]);
+
+  useEffect(() => {
+    (async () => {
+      const responseSymbols = await axiosInstance.get(
+        'https://localhost:44300/api/trading/symbols/USDT'
+      );
+
+      const loadedSymbols = (responseSymbols as any).data.symbols;
+      setSymbols(loadedSymbols);
+    })();
   }, []);
 
   if (estao) {
@@ -131,7 +104,12 @@ const CryptoDemo = (props: Props) => {
             />
           </LineChart>
         </ResponsiveContainer> */}
-        <Chart candleData={estao.candles} />
+        <Chart
+          currentSymbol={currentSymbol}
+          candleData={estao.candles}
+          updateDataCallback={setCurrentSymbol}
+          symbols={symbols}
+        />
       </div>
     );
   }
